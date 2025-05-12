b53af80b284abc37d86f8fe0c3c734b5e3ef30c8
jacobchase <jchase996@gmail.com>
2025-05-12 15:25:02 -0500

    set text to auto-update in captions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import Header from "/src/components/header"
 import Body from "/src/components/body"
-import './App.css'
 
 export default function App() {
  
--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,4 +1,17 @@
+import React from "react"
+
 export default function Body(){
+
+    
+    const [meme,setMeme] = React.useState({topText:"One does not simply", bottomText:"walk into Mordor", imageURL:"http://i.imgflip.com/1bij.jpg"})
+    
+    function handleChange(event){
+            const {value, name} = event.currentTarget
+            setMeme(prevInfo => 
+                ({...prevInfo, [name]: value})
+            )
+    }
+
     return (
 <main>
             <div className="form">
@@ -7,22 +20,26 @@ export default function Body(){
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
                     />
                 </label>
 
                 <label>Bottom Text
                     <input
                         type="text"
-                        placeholder="Walk into Mordor"
+                        placeholder="walk into Mordor"
                         name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={meme.imageURL} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
         )
